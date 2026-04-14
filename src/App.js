2a1088b0cc4d09d3898cd28ebbcea2abcbaab219
hansa <hansaverma175@gmail.com>
2026-04-14 20:47:02 +0530

    added alumni,attendance, rent pages

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import LoginPage from './pages/LoginPage';
 import HomePage from './pages/HomePage';
+import Accommodations from './pages/Accommodations';
+import Alumni from './pages/Alumni';
+import MyPage from './pages/MyPage';
+
 
 function App() {
   return (
@@ -8,6 +12,10 @@ function App() {
       <Routes>
         <Route path="/" element={<LoginPage />} />
         <Route path="/home" element={<HomePage />} />
+        <Route path="/accommodations" element={<Accommodations />} />
+        <Route path="/mypage" element={<MyPage />} />
+        <Route path="/alumni" element={<Alumni />} />
+        
       </Routes>
     </BrowserRouter>
   );
